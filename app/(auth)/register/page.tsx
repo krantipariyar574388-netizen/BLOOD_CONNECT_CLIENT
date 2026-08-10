5561f6a93804ff64f2b4c6944c809f6cb48bd719
krantipariyar574388-netizen <krantipariyar574388@gmail.com>
2026-08-10 20:30:13 +0545

    feat: add login page with email and password form

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Register | Blood Connect App",
+  description: "Blood connect App register Page",
+};
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <h1>Register Page</h1>
+      </div>
+  )
+}
+
+export default RegisterPage
